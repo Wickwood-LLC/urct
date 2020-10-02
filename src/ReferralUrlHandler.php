ab697a3a2652f054c3f4e264115fceeb0d1dd7e9
Junaid PV <junu.pv@gmail.com>
2020-10-02 22:46:39 +0530

    Not to act on user pages.

--- src/ReferralUrlHandler.php
+++ src/ReferralUrlHandler.php
@@ -67,6 +67,9 @@ class ReferralUrlHandler implements InboundPathProcessorInterface {
    * {@inheritdoc}
    */
   public function processInbound($path, Request $request) {
+    if (strpos($path, '/user/') === 0) {
+      return $path;
+    }
     $result = static::getReferralFromPath($path);
     if ($result) {
       $path = $result->normal_path;
